3a3366f0f259c167cf7f4201f9d0857e5834fdc1
Francesco Ronconi <franciron@hotmail.it>
2019-03-04 11:43:12 +0100

    add approximate_size func

---
+++ humansize.py
@@ -0,0 +1,10 @@
+SUFFIXES = ['KB', 'MB', 'GB', 'PB', 'EB', 'ZB', 'YB']
+
+def appriximate_size(size):
+    multiple = 1024
+    for suffix in SUFFIXES:
+        size /= multiple
+        if size < multiple:
+            return '{} {}'.format(size, suffix)
+
+            # format string
